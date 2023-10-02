67d0d9216f862024c0edc6b54ea317922edb320b
Laptop <nagraham4@gmail.com>
2023-10-02 09:44:25 -0400

    overview updated

--- Progress_Blog/overview.js
+++ Progress_Blog/overview.js
@@ -7,3 +7,39 @@ document.getElementById("cards").onmousemove = (e) => {
     card.style.setProperty("--mouse-y", `${y}px`);
   }
 };
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+const container = document.getElementById("glitch-container");
+const glitchElement = document.getElementById("glitch-effect");
+
+container.addEventListener("mouseover", () => {
+  let iteration = 0;
+  glitchElement.classList.add("red-glow");
+  glitchElement.classList.remove("aquamarine-glow");
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    glitchElement.innerHTML = glitchElement.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return `<span class="aquamarine-glow">${glitchElement.dataset.value[index]}</span>`;
+        } else if (index == 5 || index == 9) {
+          return " ";
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= glitchElement.dataset.value.length) {
+      clearInterval(interval);
+      glitchElement.classList.remove("red-glow");
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+});
