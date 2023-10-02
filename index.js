3fe831b8c69752069501da1a0361a67a5ac26af3
Nic <nagraham4@gmail.com>
2023-10-01 21:54:33 -0400

    first box made

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ document.getElementById("cards").onmousemove = (e) => {
     card.style.setProperty("--mouse-y", `${y}px`);
   }
 };
+
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+const container = document.getElementById("glitch-container");
+const glitchElement = document.getElementById("glitch-effect");
+
+// document.getElementById("glitch-effect").onmouseover = (event) => {
+container.addEventListener("mouseover", () => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    glitchElement.innerText = glitchElement.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return glitchElement.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= glitchElement.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+});
