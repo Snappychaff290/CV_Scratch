211449b6da7e4324f4b774217ea4ce3059749603
Nic <nagraham4@gmail.com>
2023-10-13 14:42:48 -0400

    ChatGPT implementation

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ document.getElementById("cards").onmousemove = (e) => {
 };
 
 // CHATBOT
+async function fetchTextFile(filePath) {
+  const response = await fetch(filePath);
+  const text = await response.text();
+  return text;
+}
+
 function toggleChat() {
   var chatPopup = document.getElementById("chat");
   var chatContainer = document.getElementById("chat-popup");
@@ -22,10 +28,37 @@ function toggleChat() {
   }
 }
 
-function changeText() {
-  var paragraph = document.getElementById("answer");
-  paragraph.innerHTML = "New text after button click.";
+async function changeText() {
+  try {
+    var filePath = "info.txt"; // Update with the path to your .txt file
+    var text = await fetchTextFile(filePath);
+    var question = text + document.getElementById("user-input").value;
+    var paragraph = document.getElementById("answer");
+    paragraph.innerHTML = "Loading..."; // Notify the user that the request is being processed
+
+    const response = await fetch("http://localhost:3000/ask-question", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ question, model: "gpt-3.5-turbo" }), // Include the model parameter
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result); // Log the result object to the console
+    const answer = result.choices[0].message.content;
+
+    paragraph.innerHTML = answer; // Display the extracted text
+  } catch (error) {
+    console.error("Error:", error);
+    paragraph.innerHTML = "An error occurred. Please try again later."; // Display an error message
+  }
 }
+
 // CHATBOT
 
 // const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
